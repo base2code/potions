2294d4bc7e028a02742e8e07a535becd1f9d033f
base2code <niklas.grenningloh@gmx.de>
2023-08-10 18:16:25 +0200

    initial commit

---
+++ src/main/java/de/base2code/potions/Potions.java
@@ -0,0 +1,17 @@
+package de.base2code.potions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Potions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
